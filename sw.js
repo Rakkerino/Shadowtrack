44b2007af24efe8c5476eab1264d6baeb3d6abf1
Rakkerino <kobealen@gmail.com>
2026-09-02 09:13:49 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this number whenever you upload a new index.html
-const VERSION = 'shadow-hunter-v8';
+const VERSION = 'shadow-hunter-v9';
 const FILES = [
   './', './index.html', './manifest.json', './icon-192.png', './icon-512.png',
   './sprites/adder.png',
